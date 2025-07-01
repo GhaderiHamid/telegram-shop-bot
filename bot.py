5752c0cef19308f091771a4493cd021a4e209987
hamid <ghaderihamid86@gmail.com>
2025-07-01 14:26:16 +0330

    ch1

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
+DB_PORT=os.getenv("DB_PORT")
 PAYMENT_SERVICE_URL = os.getenv("PAYMENT_SERVICE_URL", "https://hamidstore.liara.run/payment")
 
 # Database connection
@@ -23,7 +24,8 @@ def get_db_connection():
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
-        database=DB_NAME
+        database=DB_NAME,
+        port=DB_PORT,
     )
 
 db = get_db_connection()
